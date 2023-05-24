db19f525a10efcf20020ecece5b91531a3e19e98
Rokoshan <133026403+Komkrit-Bank@users.noreply.github.com>
2023-05-24 21:46:41 +0700

    Complete Change Theme

--- myHomeWork/src/App.tsx
+++ myHomeWork/src/App.tsx
@@ -1,9 +1,42 @@
+import { useForm } from 'react-hook-form'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import './scss/style.scss'
 import './App.css'
-import Water from './components/Water'
+import Water from './components/Water'  
+
+type Inputs = {theme: string};
+document.body.classList.add('light_mode');
 
 function App() {
+  const [theme, setTheme] = useState('light_mode');
+  const { register, handleSubmit } = useForm<Inputs>();
+  
+
+  const changeTheme = (theme: string)=> {
+    document.body.classList.value = theme
+  }
+
+  const onChange = (data:any) => {
+    setTheme(data.theme);
+    changeTheme(data.theme)
+  }
+  
+
   return (
-    <Water/>
+    <div className= 'app'>
+      <Water theme= { theme }/>
+      <div className="theme_container">
+          <FontAwesomeIcon icon={theme === 'light_mode'? faSun : faMoon} />
+            <form onChange={handleSubmit(onChange)} className="">
+                <select {...register("theme")} className="form-select">
+                    <option value="light_mode">Light</option>
+                    <option value="dark_mode">Dark</option>
+                </select>
+            </form>
+      </div>
+    </div>
   )
 }
 
--- myHomeWork/src/components/Water.tsx
+++ myHomeWork/src/components/Water.tsx
@@ -1,14 +1,16 @@
 import { useForm } from 'react-hook-form'
 import { useState } from 'react';
-import Theme from './Theme';
 import '../scss/style.scss'
 
 type Inputs = {
-    weight: number;
+    weight: number,
+    theme: string
 };
 
-const Water = ()=>{
+const Water = (props: any)=>{
+    const { theme } = props
     const [result, setResult] = useState(0);
+
     const waterCal = (weight: any) => {
         const result = weight * 2.2 * 30/2
         return weight === 0? 'x' : result
@@ -16,13 +18,21 @@ const Water = ()=>{
 
     const { register, handleSubmit } = useForm<Inputs>();
     const onSubmit = (data:any) => setResult(data.weight)
+
+    const card_theme = (data: any) => {
+        if (data === 'light_mode'){
+            return "card border border-warning border-3" + theme + '--card';
+        } else {
+            return "card border border-white border-3" + theme + '--card';
+        }
+    }
     
     return(
         <>
-            <div className="card border border-warning border-3">
+            <div className= {card_theme(theme)}>
                 <div className="card-body">
                     <div className="card-title m-3">
-                        <h2 className='form-label' style={{color: 'orange', fontWeight: 'bold'}}>ควรดื่มน้ำวันละเท่าไหร่ ?</h2>
+                        <h2 className={'form-label '+ theme +'--text'}>ควรดื่มน้ำวันละเท่าไหร่ ?</h2>
                         <h3 className='form-label'>{waterCal(result).toLocaleString("en-US")} มล.</h3>
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)}>
@@ -31,7 +41,6 @@ const Water = ()=>{
                     </form>
                 </div>
             </div>
-            <Theme/>
         </>
     )
 }
